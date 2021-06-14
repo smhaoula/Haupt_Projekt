ad70f09758938bab2bd914e87a2a259e49d83127
smhaoula <hamza.oulahrir@tu-dortmund.de>
2021-06-14 18:43:26 +0200

    State machine

--- Haupt Game Projekt/Assets/Scripts/EndlessTerrain.cs
+++ Haupt Game Projekt/Assets/Scripts/EndlessTerrain.cs
@@ -75,10 +75,10 @@ public class EndlessTerrain : MonoBehaviour
             }
 
         }
-      /*  for (int i = 0; i < Surfaces.Length; i++)
-        {
-            Surfaces[i].BuildNavMesh();
-        }*/
+        //for (int i = 0; i < Surfaces.Length; i++)
+        //{
+        //    Surfaces[i].BuildNavMesh();
+        //}
     }
 
 
--- Haupt Game Projekt/Assets/Scripts/Enemy Ki/Enemyki.cs
+++ Haupt Game Projekt/Assets/Scripts/Enemy Ki/Enemyki.cs
@@ -9,20 +9,27 @@ using UnityEngine.AI;
 public class Enemyki : MonoBehaviour
 {
     public Transform Player;
-   
+    public NavMeshTriangulation Triangulation;
+    public EnemyLineOfSightChecker LineOfSightChecker;
     [SerializeField]
     private Animator Animator = null;
     public float UpdateRate = 0.1f;
     Transform target;
     NavMeshAgent Agent;
     Animator anim;
-  
-    
+
+
+    public Vector3[] Waypoints = new Vector3[4];
+    [SerializeField]
+    private int WaypointIndex = 0;
+
 
     private const string iswalking = "iswalking";
     
 
     private Coroutine FollowCoroutine;
+    public float IdleLocationRadius = 4f;
+    public float IdleMovespeedMultiplier = 0.5f;
 
     public EnemyState DefaultState;
     private EnemyState _state;
@@ -45,11 +52,53 @@ public class Enemyki : MonoBehaviour
     {
         
         Agent = GetComponent<NavMeshAgent>();
-        
+        Triangulation = NavMesh.CalculateTriangulation();
+
 
         OnStateChange += HandleStateChange;
 
+        LineOfSightChecker.OnGainSight += HandleGainSight;
+        LineOfSightChecker.OnLoseSight += HandleLoseSight;
+
+
+        OnStateChange?.Invoke(EnemyState.Spawn, DefaultState);
+
+
     }
+
+    private void Start()
+    {
+        Triangulation = NavMesh.CalculateTriangulation();
+        Spawn();
+    }
+    private void HandleGainSight(Player player)
+    {
+        State = EnemyState.Chase;
+    }
+
+    private void HandleLoseSight(Player player)
+    {
+        State = DefaultState;
+    }
+    public void Spawn()
+    {
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            NavMeshHit Hit;
+            if (NavMesh.SamplePosition(Triangulation.vertices[Random.Range(0, Triangulation.vertices.Length)], out Hit, 2f, Agent.areaMask))
+            {
+                Waypoints[i] = Hit.position;
+            }
+            else
+            {
+                Debug.LogError("Unable to find position for navmesh near Triangulation vertex!");
+            }
+        }
+        OnStateChange?.Invoke(EnemyState.Spawn, DefaultState);
+    }
+
+
+
     private void Update()
     {
         Animator.SetBool(iswalking, Agent.velocity.magnitude > 0.01f);
@@ -72,18 +121,18 @@ public class Enemyki : MonoBehaviour
                 StopCoroutine(FollowCoroutine);
             }
 
-            /*if (oldState == EnemyState.Idle)
+            if (oldState == EnemyState.Idle)
             {
                 Agent.speed /= IdleMovespeedMultiplier;
-            }*/
+            }
 
             switch (newState)
             {
                 case EnemyState.Idle:
-                   // FollowCoroutine = StartCoroutine(DoIdleMotion());
+                     FollowCoroutine = StartCoroutine(DoIdleMotion());
                     break;
                 case EnemyState.Patrol:
-                   // FollowCoroutine = StartCoroutine(DoPatrolMotion());
+                     FollowCoroutine = StartCoroutine(DoPatrolMotion());
                     break;
                 case EnemyState.Chase:
                     FollowCoroutine = StartCoroutine(FollowTarget());
@@ -102,6 +151,72 @@ public class Enemyki : MonoBehaviour
                 Agent.SetDestination(Player.transform.position);
             }
             yield return Wait;
+        }
+    }
+
+    private IEnumerator DoIdleMotion()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+
+        Agent.speed *= IdleMovespeedMultiplier;
+
+        while (true)
+        {
+            if (!Agent.enabled || !Agent.isOnNavMesh)
+            {
+                yield return Wait;
+            }
+            else if (Agent.remainingDistance <= Agent.stoppingDistance)
+            {
+                Vector2 point = Random.insideUnitCircle * IdleLocationRadius;
+                NavMeshHit hit;
+
+                if (NavMesh.SamplePosition(Agent.transform.position + new Vector3(point.x, 0, point.y), out hit, 2f, Agent.areaMask))
+                {
+                    Agent.SetDestination(hit.position);
+                }
+            }
+
+            yield return Wait;
+        }
+    }
+    private IEnumerator DoPatrolMotion()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+
+        yield return new WaitUntil(() => Agent.enabled && Agent.isOnNavMesh);
+        Agent.SetDestination(Waypoints[WaypointIndex]);
+
+        while (true)
+        {
+            if (Agent.isOnNavMesh && Agent.enabled && Agent.remainingDistance <= Agent.stoppingDistance)
+            {
+                WaypointIndex++;
+
+                if (WaypointIndex >= Waypoints.Length)
+                {
+                    WaypointIndex = 0;
+                }
+
+                Agent.SetDestination(Waypoints[WaypointIndex]);
+            }
+
+            yield return Wait;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Gizmos.DrawWireSphere(Waypoints[i], 0.25f);
+            if (i + 1 < Waypoints.Length)
+            {
+                Gizmos.DrawLine(Waypoints[i], Waypoints[i + 1]);
+            }
+            else
+            {
+                Gizmos.DrawLine(Waypoints[i], Waypoints[0]);
+            }
         }
     }
 }
